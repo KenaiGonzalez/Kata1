4973edc74bf630191e5df994cc9ee8aa1f7caa06
kenai <kenai@LAPTOP-JP8VSD3S>
2021-09-29 13:27:13 +0100

    Primera versión de Kata1

---
+++ src/kata1/Main.java
@@ -0,0 +1,18 @@
+package kata1;
+
+import java.util.Date;
+
+public class Main {
+    
+    
+
+    public static void main(String[] args) {
+        Person person1 = new Person("Antonio",new Date(49,8,24));
+        System.out.println(person1.getName() + " tiene " + person1.getAge() + " años.");
+    }
+    
+}
+    
+
+    
+    
